dfdef3212c27d7eb0905844c4e8d04b918e6dcf4
Yevhen <yevhen.yakymenko.fed@gmail.com>
2022-10-18 00:20:47 +0300

    fixed bag with api

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import PropTypes from 'prop-types';
 
 export const getImages = async (request, page = 1) => {
-  const key = process.env.REACT_APP_PIXABAY_API_KEY;
+  // const key = process.env.REACT_APP_PIXABAY_API_KEY;
+  const key = '30596493-cea9f55333817c2aaa883ff0b';
   // const url = `https://pixabay.com/api/?q=${request}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=4`;
   const url = `https://pixabay.com/api/?q=${request}&page=${page}&key=${key}&orientation=horizontal&per_page=8`;
 
