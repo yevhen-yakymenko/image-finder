665331de0efcb43dabd93b10908166abf74371b2
Yevhen <yevhen.yakymenko.fed@gmail.com>
2022-10-20 22:14:03 +0300

    code formating

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from 'components/App';
+
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const rootContainer = document.getElementById('root');
+const root = createRoot(rootContainer);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import PropTypes from 'prop-types';
 
-export const getImages = async (request, page = 1) => {
+export const getImages = async (request, page = 1, perPage = 8) => {
   // const key = process.env.REACT_APP_PIXABAY_API_KEY;
   const key = '30596493-cea9f55333817c2aaa883ff0b';
   // const url = `https://pixabay.com/api/?q=${request}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=4`;
-  const url = `https://pixabay.com/api/?q=${request}&page=${page}&key=${key}&orientation=horizontal&per_page=8`;
+  const url = `https://pixabay.com/api/?q=${request}&page=${page}&key=${key}&orientation=horizontal&per_page=${perPage}`;
 
   const responce = await axios.get(url);
 
@@ -15,4 +15,16 @@ export const getImages = async (request, page = 1) => {
 getImages.propTypes = {
   request: PropTypes.string.isRequired,
   page: PropTypes.number.isRequired,
+  perPage: PropTypes.number,
 };
+
+// axios.defaults.headers.common['Authorization'] =
+//   'Bearer 433@ebfabc654a6992c2aa792f3173a3';
+// const fetchArticles = ({ searchQuery = '', currentPage = 1, pageSize = 5 }) => {
+//   return axios
+//     .get(
+//       `https://newsapi.org/v2/everything?q=${searchQuery}&pageStze=${pageSize}$page=${currentPage}`
+//     )
+
+//     .then(response => response.data.articles);
+// };
